a6226cac420ec45f1c91bebd752e8fc085e9540e
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-04-29 12:44:02 +0530

    updated to new footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,19 +2,25 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer className="container-fluid">
-        <div className="container-center">
-          <div className="brand clr-light">Fun Triangle</div>
-          <span className="my-1 clr-light d-block">
-            Social links of the developer
-          </span>
-          <ul className="list-no-bullet list-social-links">
-            <li className="list-item-inline"> <a className="link link-text-dec-none" href="https://www.instagram.com/jit2endra/">Instagram</a></li> |
-            <li className="list-item-inline"> <a className="link link-text-dec-none" href="https://www.linkedin.com/in/jitendra1mp4/">Linkedin</a></li> |
-            <li className="list-item-inline"> <a className="link link-text-dec-none" href="https://github.com/jitendra1mp4">Github</a></li>
-          </ul>
-        </div>
-      </footer>
+      <footer className="container-fluid">
+      <div className="container-center">
+        <div className="brand clr-light">Jitendra Kumar Sahu</div>
+        <span className="my-1 clr-light d-block">
+          Social Links
+        </span>
+        <ul className="list-no-bullet list-social-links">
+          <li className="list-item-inline">
+            <a className="link-text-dec-none link-social" href="https://www.instagram.com/jit2endra/">Instagram</a>
+          </li> |
+          <li className="list-item-inline">
+            <a className="link-social link-text-dec-none" href="https://www.linkedin.com/in/jitendra1mp4/">Linkedin</a>
+          </li> |
+          <li className="list-item-inline"> <a className="link-social link-text-dec-none"
+              href="https://github.com/jitendra1mp4">Github</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
       
     ) ;
 }
