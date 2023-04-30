b4fc2280f4b833ae8f1a2b45cdea28087e7df9d1
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-04-30 15:11:08 +0530

    fix : home section btn were not working correctly.

--- src/Components/Sections/SectionHome.js
+++ src/Components/Sections/SectionHome.js
@@ -14,7 +14,7 @@ export default function SectionHome(props) {
         <a
           className={`btn btn-${props.index === 0 ? "primary" : "secondary"} link-text-dec-none`}
           
-          onClick={() => props.updatePage(getLink, props.index + 1)}
+          onClick={() => props.updatePage(getLink(), props.index + 1)}
         >
           {props.btnPrimary.content}
         </a>
