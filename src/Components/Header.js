db7ca819bba60d63fa8c619ee79638c446423685
Jitendra Kumar Sahu <70005117+Jitendra1mp4@users.noreply.github.com>
2023-05-01 19:05:23 +0530

    fix : added alt in Img tags

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header(props) {
   return (
     <header id="header">
-      <img className="hero-img" src={props.headerImage} />
+      <img className="hero-img" src={props.headerImage} alt="page illustration figs" />
       <h1 className="hero-text">
         {props.headerText}
       </h1>
